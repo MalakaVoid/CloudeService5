c7c97e5ff6262a2c4ab04706c2352548632f9776
dazbukin <dazbukin@mbs.media>
2024-11-18 19:29:10 +0300

    prometeus metrics

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_migrate import Migrate
 from flask_caching import Cache
-from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 app.config.from_object(Config)
-metrics = PrometheusMetrics(app)
 
 cache = Cache(app)
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from config import Config
 from app import app, db
+from prometheus_flask_exporter import PrometheusMetrics
 
 
 with app.app_context():
@@ -7,5 +8,6 @@ with app.app_context():
 
 
 if __name__ == '__main__':
+    metrics = PrometheusMetrics(app)
     app.run(host=Config.FLASK_APP_HOST,
             debug=Config.FLASK_DEBUG)
